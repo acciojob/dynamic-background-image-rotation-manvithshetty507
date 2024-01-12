086b54cbb0d0aea94c75e890ea5fc042d3ee4856
Manvith Kumar shetty <121666002+manvithshetty507@users.noreply.github.com>
2024-01-12 14:54:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let width = window.innerWidth;
+let height = window.innerHeight;
+
+if (width > height) {
+    document.body.style.backgroundImage = 'url("https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Paysage-en-orientation-paysage.jpg")';
+} else {
+    document.body.style.backgroundImage = 'url("https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Portrait-en-orientation-portrait.jpg")';
+}
